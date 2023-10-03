133b67447be67f0e88037646f1bb6ce798cd6e7d
Takumin <c0a2214705@edu.teu.ac.jp>
2023-10-03 14:27:34 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -8,11 +9,20 @@ WIDTH, HEIGHT = 1600, 900
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex02/fig/3.png")
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
+
+    bd_img = pg.Surface((20,20))
+    bd_img.set_colorkey((0,0,0))
+    pg.draw.circle(bd_img, (255,0,0),(10, 10), 10)
+    bd_rct = bd_img.get_rect() # Surfaceからrectを抽出する
+    x, y = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    bd_rct.center = x, y # rectにランダムな座標を設定する
+    vx,vy = +5, +5
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -20,9 +30,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx, vy)
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
